bf9fed805e2a790fded6a979bea2ea82e8328383
stefania <stefania.pozzi.it@gmail.com>
2025-06-26 21:57:18 +0200

    removed rateLimitProvider

--- src/main/kotlin/com/example/demo/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/SecurityConfig.kt
@@ -7,11 +7,11 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent
 import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.ObjectPostProcessor
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.access.intercept.AuthorizationFilter
 
 @Configuration
 @EnableWebSecurity
