fe8deccda15611dc6a6d7a568f521a97a03e5ae1
stefania <stefania.pozzi.it@gmail.com>
2025-07-01 16:16:21 +0200

    removed unused imports

--- src/main/kotlin/com/example/demo/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/SecurityConfig.kt
@@ -3,22 +3,12 @@
     import org.springframework.context.ApplicationListener
     import org.springframework.context.annotation.Bean
     import org.springframework.context.annotation.Configuration
-    import org.springframework.security.authentication.AuthenticationManager
-    import org.springframework.security.authentication.ProviderManager
-    import org.springframework.security.authentication.dao.DaoAuthenticationProvider
     import org.springframework.security.authentication.event.AuthenticationSuccessEvent
     import org.springframework.security.config.Customizer
     import org.springframework.security.config.annotation.web.builders.HttpSecurity
     import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-    import org.springframework.security.core.userdetails.UserDetailsService
-    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-    import org.springframework.security.crypto.password.DelegatingPasswordEncoder
-    import org.springframework.security.crypto.password.PasswordEncoder
-    import org.springframework.security.provisioning.InMemoryUserDetailsManager
     import org.springframework.security.web.SecurityFilterChain
-    import org.springframework.security.web.access.intercept.AuthorizationFilter
     import org.springframework.security.web.authentication.AuthenticationFilter
-    import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 
     @Configuration
     @EnableWebSecurity
