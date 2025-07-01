5003658aa03ee8017285dc272f327b720f10195b
stefania <stefania.pozzi.it@gmail.com>
2025-07-01 08:02:54 +0200

    set httpbasic configuration

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 demo-basic-private:
-	curl -vu user:xyz http://localhost:8080/private
+	curl -vu user:7359e75a-565b-4798-8b7b-ba0226f42fb1 http://localhost:8080/private
 
 
--- src/main/kotlin/com/example/demo/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -17,6 +18,7 @@ class SecurityConfig {
                 c -> c.requestMatchers("/private").authenticated()
                 .anyRequest().permitAll()
             }
+            .httpBasic(Customizer.withDefaults())
         return http.build()
     }
 }
