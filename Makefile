51156179caa3cb2eba341b4a001e96f58d8e7a31
stefania <stefania.pozzi.it@gmail.com>
2025-07-01 08:34:51 +0200

    custom users

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+## 01 autentication-flow
 demo-basic-private:
 	curl -vu user:7359e75a-565b-4798-8b7b-ba0226f42fb1 http://localhost:8080/private
 
-
+## 02 user-details
+demo-users:
+	curl -vu rickGrimes:sheriff123 http://localhost:8080/private
---
+++ src/main/kotlin/com/example/demo/UserConfig.kt
@@ -0,0 +1,27 @@
+package com.example.demo
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
+
+@Configuration
+class UserConfig {
+
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val rickGrimes = User.withUsername("rickGrimes")
+            .password("{noop}sheriff123")
+            .roles("LEADER")
+            .build()
+
+        val darylDixon = User.withUsername("darylDixon")
+            .password("{noop}crossbow456")
+            .roles("HUNTER")
+            .build()
+
+        return InMemoryUserDetailsManager(rickGrimes, darylDixon)
+    }
+}
